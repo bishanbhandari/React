0a57f41771bb19c3a12e956e256dc4bf684bc278
Bishan Bhandari <108720684+bishanbhandari@users.noreply.github.com>
2025-05-26 15:24:07 +0545

    Props

---
+++ Props/src/App.jsx
@@ -0,0 +1,24 @@
+/*props=Read-only properties that are shared between components.
+        A parent component can send data to child component 
+        <component key=value/>*/
+
+/*propTypes=A mechanism that ensures that the passed value is of the correct datatype
+            Age:propTypes.number */  
+            
+/*defaultProps=default values for props in case they are not passed from the parent component
+                name:"Guest"*/ 
+
+import Student from "./Student"
+
+    function App(){
+      return (
+        <>
+           <Student name="spongebob" age={30} isStudent={true}/>
+           <Student name="Patrick" age={40} isStudent={false} />
+           <Student name="Squidward" age={50} isStudent={false} />
+           <Student name="Goku" age={50} isStudent={false} />
+           <Student name="Larray" />
+        </>
+      );
+    }
+    export default App
